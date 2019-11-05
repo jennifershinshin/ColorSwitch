78d16bc70d3c6b936e170639374c720cce40ad91
jennifershinshin <jennifershinshin@gmail.com>
2019-11-04 16:42:47 -0800

    Some progress on infinite scroller. Instantiated children of Level when Level awakes

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    private static Level _instance;
+    private Transform colorChanger;
+    private Transform smallCircle;
+
+    public GameObject smallCirclePrefab;
+    public GameObject colorChangerPrefab;
+
+    public static Level Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+        //do i have to create its child objects
+        setColorChanger();
+        setSmallCircle();
+    }
+    private void Start()
+    {
+        
+    }
+    void setColorChanger()
+    {
+        if(GameObject.FindWithTag("ColorChanger"))
+        {
+            colorChanger = GameObject.FindWithTag("ColorChanger").GetComponent<Transform>();
+        }
+        else
+        {
+            GameObject colorChangerGO = Instantiate(colorChangerPrefab) as GameObject;
+            colorChanger = colorChangerGO.GetComponent<Transform>();
+        }
+    }
+
+    public Transform getColorChanger()
+    {
+        return colorChanger;
+    }
+
+    void setSmallCircle()
+    {
+        if (GameObject.FindWithTag("SmallCircle"))
+        {
+            smallCircle = GameObject.FindWithTag("SmallCircle").GetComponent<Transform>();
+        }
+        else
+        {
+            GameObject smallCircleGO = Instantiate(smallCirclePrefab) as GameObject;
+            smallCircle = smallCircleGO.GetComponent<Transform>();
+        }
+    }
+
+    public Transform getSmallCircle()
+    {
+        return smallCircle;
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject Level;
+    public Transform camera;
+    public Transform player;
+
+    private void Awake()
+    {
+        Level = GameObject.FindWithTag("Level");
+
+        //find Level(contains smallCircle and colorChanger)
+        // find colorChanger. get position of colorChanger.
+        //maybe only have one colorChanger in world
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!getColorChanger())
+        {
+            Debug.Log("nonono");
+            spawnLevel();
+        }
+    }
+
+    void spawnLevel()
+    {
+        //need position maybe before it gets destroyed. maybe at level's awake
+          Instantiate(Level, player.position, Quaternion.identity);
+    }
+
+    Transform getColorChanger()
+    {
+        return Level.GetComponent<Level>().getColorChanger();
+    }
+    Transform getSmallCircle ()
+    {
+        return Level.GetComponent<Level>().getSmallCircle();
+    }
+}
